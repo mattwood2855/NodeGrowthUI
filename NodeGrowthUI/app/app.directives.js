449613a055ceef69953e4aee9f61b28d6e859f8e
Matthew Wood <mattwood2855@gmail.com>
2016-08-13 09:54:03 -0700

    Initial Check In

---
+++ NodeGrowthUI/app/app.directives.js
@@ -0,0 +1,24 @@
+﻿var app = angular.module('NodeGrowth', []);
+
+app.directive('topNavBar', function () {
+    return {
+        templateUrl: '/app/shared/topNavBar/topNavBar.html'
+    };
+});
+
+app.directive('rooms', function () {
+    return {
+        restrict: 'E',
+        templateUrl: '/app/components/rooms/templates/rooms.html',
+        controller: ['$scope', function ($scope) {
+
+            console.log("test");
+        }]
+    };
+});
+
+app.directive('settings', function () {
+    return {
+        templateUrl: '/app/components/settings/templates/settings.html'
+    };
+});
---
+++ NodeGrowthUI/app/app.js
@@ -0,0 +1,10 @@
+﻿var app = angular.module('NodeGrowth', []);
+
+app.controller("NodeGrowth.rooms", function ($scope) {
+    var test = '1';
+});
+
+app.controller("NodeGrowth.topNavBarController", function ($scope) {
+    var test = '1';
+});
+
